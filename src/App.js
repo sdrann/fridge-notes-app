c87cf55fc4403a468da48711f20759964f2a5ad9
SandraVatr <sandravatr@gmail.com>
2023-03-13 15:49:37 +0200

    responsive grid and note card size

--- src/App.js
+++ src/App.js
@@ -12,20 +12,45 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+import CircleIcon from '@mui/icons-material/Circle';
+import AnchorIcon from '@mui/icons-material/Anchor';
+
+import Note from './Note';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#91e0ff' : '#cc9cff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
+  justifyContent: 'center',
   color: theme.palette.text.secondary,
 }));
+
+
+const a = [
+  {
+    name: "Tim",
+    age: 27
+  },
+  {
+    name: "Bob",
+    age: 32
+  },
+  {
+    name: "Ann",
+    age: 27
+  },
+  {
+    name: "Zen",
+    age: 27
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Box sx={{ flexGrow: 2 }}>
+      {/* <header className="App-header"> */}
+      {/* <Box sx={{ flexGrow: 2 }}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
           <Item>xs=8</Item>
@@ -37,18 +62,44 @@ function App() {
           <Item>xs=4</Item>
         </Grid>
         <Grid item xs={8}>
-          <Item>xs=8</Item>
+          <Item>xs=8</Item> 
         </Grid>
+      </Grid>
+    </Box> */}
+    
+   
+    <AnchorIcon fontSize='large'></AnchorIcon>
+    <container> 
+    <div sx={{textAlign: 'right', marginRight: '0px', justifyContent: 'right'}}>
+      <h2>ABOUT</h2>
+    </div>
+    </container>
+    {/* <Note text="AAAAAAAA"/>   */}
+    <Box sx={{justifyContent:"center"}}>
+      <Grid container spacing={2}>
+        {a.map((itemm, index) => (
+          <Grid item xs={12} md={6}>
+            {/* <Item>  */}
+              <div key={index}>
+                <Note text={itemm.name}/>
+              </div>
+            {/* </Item> */}
+          </Grid>
+        ))}
       </Grid>
     </Box>
 
+
+
+   
+    
+    {/* <div sx={{ margin: "0em" }}>
+
     <Card sx={{ minWidth: 275, margin: "1em" }}>
+      <CircleIcon style={{color:"red", marginTop: "0.5em"}}></CircleIcon>
       <CardContent>
         <Typography variant="h5" component="div">
           Groceries
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
         </Typography>
         <Typography variant="body2">
           well meaning and kindly.
@@ -57,11 +108,33 @@ function App() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+      <Button variant="text">Text</Button>
       </CardActions>
     </Card>
-      </header>
+
+    <Card sx={{ minWidth: 275, margin: "1em" }}>
+    <CircleIcon style={{color:"red", marginTop: "0.5em"}}></CircleIcon>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          Groceries
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Button variant="text">Text</Button>
+      </CardActions>
+    </Card>
+
+    </div> */}
+
+      {/* </header> */}
     </div>
+
+   
   );
 }
 
---
+++ src/Note.js
@@ -0,0 +1,35 @@
+import './Note.css';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import CircleIcon from '@mui/icons-material/Circle';
+import AnchorIcon from '@mui/icons-material/Anchor';
+
+function Note(props) {
+  return (
+    <div className="Note">
+      <Card  className='card' sx={{ width: '100%', maxWidth: 450, margin: "1em", boxShadow: 10 }}>
+        <CircleIcon style={{color:"red", marginTop: "0.5em", fontSize: 40}}></CircleIcon>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            <h3>{props.text}</h3>
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="text">Text</Button>
+        </CardActions>
+    </Card>
+    </div>
+  );
+}
+
+export default Note;
